2db66955d65043807394845d4c35ee1d3026b321
João Pedro <joaopedroorem@gmail.com>
2026-04-29 21:21:25 -0300

    Ribbon load balancer

--- hr-payroll/src/main/java/com/springboot/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/springboot/hrpayroll/services/PaymentService.java
@@ -4,12 +4,8 @@ import com.springboot.hrpayroll.entities.Payment;
 import com.springboot.hrpayroll.entities.Worker;
 import com.springboot.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
--- hr-worker/src/main/java/com/springboot/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/springboot/hrworker/resources/WorkerResource.java
@@ -2,7 +2,11 @@ package com.springboot.hrworker.resources;
 
 import com.springboot.hrworker.entities.Worker;
 import com.springboot.hrworker.services.WorkerService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +19,11 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkerService service;
 
@@ -26,6 +35,8 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+        logger.info("PORT = " + env.getProperty("local.server.port"));
+
         Worker obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
