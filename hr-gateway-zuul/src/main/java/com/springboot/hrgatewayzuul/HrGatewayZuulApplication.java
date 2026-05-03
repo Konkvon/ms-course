0688f1f2c3187e26701c2247b829edbda7f19641
João Pedro <joaopedroorem@gmail.com>
2026-05-03 02:12:50 -0300

    Project hr-gateway-zuul created

---
+++ hr-gateway-zuul/src/main/java/com/springboot/hrgatewayzuul/HrGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.hrgatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrGatewayZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrGatewayZuulApplication.class, args);
+    }
+
+}
---
+++ hr-gateway-zuul/src/test/java/com/springboot/hrgatewayzuul/HrGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.hrgatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrGatewayZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
